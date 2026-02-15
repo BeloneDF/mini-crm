7e42c9820d30539f1207c5f91c2c93b02c9351c4
Belone Zorzetto Fraga <belone.fraga@galax-ia.com>
2026-02-15 02:54:01 -0300

    fix(major-fix): fix urlsearchparams, fix forms errors, loadings, and others

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -5,7 +5,6 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
-console.log(env.VITE_ENABLE_API_DELAY)
 
 //* Simular um atraso de 500ms em todas as requisições para testes de loading states
 if (env.VITE_ENABLE_API_DELAY) {
